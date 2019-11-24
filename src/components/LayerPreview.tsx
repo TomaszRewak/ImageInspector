5b0703a0e9d02d9b947770450e8f581831785b7c
Tomasz Rewak <tomaszrewak@gmail.com>
2019-11-24 10:37:30 +0100

    Improving the cursor.

--- src/components/LayerPreview.tsx
+++ src/components/LayerPreview.tsx
@@ -28,14 +28,19 @@ export default class LayerPreview extends Component<Props, State>
 	}
 
 	public render(): React.ReactNode {
-		const mask = `radial-gradient(ellipse 80px 80px at ${this.props.x}px ${this.props.y}px, black 60px, transparent 80px)`;
+		const mask = `circle(50px at ${this.props.x}px ${this.props.y}px)`;
 
 		return (
 			<div style={{ position: 'absolute', top: 0 }}>
 				<canvas ref='canvas' style={{
-					maskImage: mask,
-					WebkitMaskImage: mask
+					clipPath: mask,
+					WebkitClipPath: mask
 				}} />
+				<svg style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%' }}>
+					<circle cx={this.props.x} cy={this.props.y} r='50' stroke='red' fill='transparent' strokeWidth='3' />
+					<line x1={this.props.x - 5} x2={this.props.x + 5} y1={this.props.y} y2={this.props.y} stroke='red' strokeWidth='1px' />
+					<line x1={this.props.x} x2={this.props.x} y1={this.props.y - 5} y2={this.props.y + 5} stroke='red' strokeWidth='1px' />
+				</svg>
 			</div>
 		);
 	}
