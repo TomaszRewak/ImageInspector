f88f15741f054085e9761d81f068ebc36522a589
Tomasz Rewak <tomaszrewak@gmail.com>
2019-12-02 19:16:32 +0100

    Splitting the shader.

---
+++ src/components/ShaderEditor.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+type Props = {}
+type State = {}
+
+export default class ShaderEditor extends Component<Props, State> {
+	public render() {
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+}
--- src/image-processing/Shader.ts
+++ src/image-processing/Shader.ts
@@ -1,39 +1,49 @@
+const vertexShader =
+	`
+attribute vec4 aVertexPosition;
+attribute vec2 aTexturePosition;
+
+varying lowp vec4 canvasPosition;
+varying lowp vec2 texturePosition;
+
+void main() {
+	gl_Position = aVertexPosition;
+	canvasPosition = aVertexPosition;
+	texturePosition = aTexturePosition;
+}
+`;
+
+const fragmentShaderPrefix =
+	`
+uniform sampler2D uSampler;
+
+varying lowp vec4 canvasPosition;
+varying lowp vec2 texturePosition;
+
+void main() {
+`;
+
+const fragmentShaderPostfix =
+	`
+}
+`;
+
 export default class Shader {
 	public readonly name: string;
 	public readonly vertexShaderSource: string;
 	public readonly fragmentShaderSource: string;
 
-	constructor(name: string, vertexShader: string, fragmentShader: string) {
+	constructor(name: string, shaderSource: string) {
 		this.name = name;
 		this.vertexShaderSource = vertexShader;
-		this.fragmentShaderSource = fragmentShader;
+		this.fragmentShaderSource = `${fragmentShaderPrefix}${shaderSource}${fragmentShaderPostfix}`;
 	}
 
 	public static get identity() {
 		return new Shader(
 			'Base image',
 			`
-				attribute vec4 aVertexPosition;
-				attribute vec2 aTexturePosition;
-
-				varying lowp vec4 canvasPosition;
-				varying lowp vec2 texturePosition;
-
-				void main() {
-					gl_Position = aVertexPosition;
-					canvasPosition = aVertexPosition;
-					texturePosition = aTexturePosition;
-				}
-			`,
-			`
-				uniform sampler2D uSampler;
-
-				varying lowp vec4 canvasPosition;
-				varying lowp vec2 texturePosition;
-
-				void main() {
-					gl_FragColor = texture2D(uSampler, texturePosition);
-				}
+				gl_FragColor = texture2D(uSampler, texturePosition);
 			`
 		);
 	}
@@ -42,39 +52,19 @@ export default class Shader {
 		return new Shader(
 			'Vertical line detector',
 			`
-				attribute vec4 aVertexPosition;
-				attribute vec2 aTexturePosition;
+				lowp vec4 sample1 = texture2D(uSampler, vec2(texturePosition.x - 0.01, texturePosition.y));
+				lowp vec4 sample2 = texture2D(uSampler, vec2(texturePosition.x + 0.01, texturePosition.y));
 
-				varying lowp vec4 canvasPosition;
-				varying lowp vec2 texturePosition;
+				lowp float diff = max(0.0, sample1.r + sample1.g + sample1.b - sample2.r - sample2.g - sample2.b) / 3.0;
 
-				void main() {
-					gl_Position = aVertexPosition;
-					canvasPosition = aVertexPosition;
-					texturePosition = aTexturePosition;
-				}
-			`,
-			`
-				uniform sampler2D uSampler;
+				lowp vec4 color = vec4(
+					diff,
+					diff,
+					diff,
+					1
+				);
 
-				varying lowp vec4 canvasPosition;
-				varying lowp vec2 texturePosition;
-
-				void main() {
-					lowp vec4 sample1 = texture2D(uSampler, vec2(texturePosition.x - 0.01, texturePosition.y));
-					lowp vec4 sample2 = texture2D(uSampler, vec2(texturePosition.x + 0.01, texturePosition.y));
-
-					lowp float diff = max(0.0, sample1.r + sample1.g + sample1.b - sample2.r - sample2.g - sample2.b) / 3.0;
-
-					lowp vec4 color = vec4(
-						diff,
-						diff,
-						diff,
-						1
-					);
-
-					gl_FragColor = color;
-				}
+				gl_FragColor = color;
 			`
 		);
 	}
@@ -83,40 +73,22 @@ export default class Shader {
 		return new Shader(
 			'Horizontal line detector',
 			`
-				attribute vec4 aVertexPosition;
-				attribute vec2 aTexturePosition;
+				lowp vec4 sample1 = texture2D(uSampler, vec2(texturePosition.x, texturePosition.y - 0.01));
+				lowp vec4 sample2 = texture2D(uSampler, vec2(texturePosition.x, texturePosition.y + 0.01));
 
-				varying lowp vec4 canvasPosition;
-				varying lowp vec2 texturePosition;
+				lowp float diff = max(0.0, sample1.r + sample1.g + sample1.b - sample2.r - sample2.g - sample2.b) / 3.0;
 
-				void main() {
-					gl_Position = aVertexPosition;
-					canvasPosition = aVertexPosition;
-					texturePosition = aTexturePosition;
-				}
-			`,
-			`
-				uniform sampler2D uSampler;
+				lowp vec4 color = vec4(
+					diff,
+					diff,
+					diff,
+					1
+				);
 
-				varying lowp vec4 canvasPosition;
-				varying lowp vec2 texturePosition;
-
-				void main() {
-					lowp vec4 sample1 = texture2D(uSampler, vec2(texturePosition.x, texturePosition.y - 0.01));
-					lowp vec4 sample2 = texture2D(uSampler, vec2(texturePosition.x, texturePosition.y + 0.01));
-
-					lowp float diff = max(0.0, sample1.r + sample1.g + sample1.b - sample2.r - sample2.g - sample2.b) / 3.0;
-
-					lowp vec4 color = vec4(
-						diff,
-						diff,
-						diff,
-						1
-					);
-
-					gl_FragColor = color;
-				}
+				gl_FragColor = color;
 			`
 		);
 	}
 }
+
+export { vertexShader, fragmentShaderPrefix, fragmentShaderPostfix }
